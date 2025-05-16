50094e5dcddc616ecbc7ab0457360f27d4784027
marksikaundi <emmanuelmark117@gmail.com>
2025-05-16 06:45:57 +0000

    ✨ Simplify middleware code formatting and ensure consistent use of double quotes

--- middleware.ts
+++ middleware.ts
@@ -12,27 +12,23 @@
 //   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 // };
 
-
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // Define public routes that should NOT be protected
-const isPublicRoute = createRouteMatcher([
-  '/api/uploadthing',
-  '/api/webhook'
-])
+const isPublicRoute = createRouteMatcher(["/api/uploadthing", "/api/webhook"]);
 
 export default clerkMiddleware(async (auth, req) => {
   // Only protect routes that are not in the public routes list
   if (!isPublicRoute(req)) {
-    await auth.protect()
+    await auth.protect();
   }
-})
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
